96cba45d4a196a979d077cae391a19d60e5c81a5
dhamo <dhamo@MODULE_LAPTOP_1>
2024-11-12 12:51:32 +0200

    adding BuildingBlocks with CQRS folder also including ICommand,ICommandHandler,IQuery,IQueryHandler adding mediatr in buldingblocks remove it from Catalog Api , in Dependences of CatalogAPI add new Project Reference Building blogs

---
+++ src/BuildingBlocks/BuildingBlocks/CQRS/ICommand.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+
+namespace BuildingBlocks.CQRS;
+
+//this one is an empty icommand
+public interface ICommand : ICommand<Unit>
+{
+
+}
+
+// this one produces a response
+public  interface ICommand<out TResponse> : IRequest<TResponse>
+{
+}
