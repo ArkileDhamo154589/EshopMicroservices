369577a7a1f06d677115d13ea184c1a10029285d
dhamo <dhamo@MODULE_LAPTOP_1>
2024-11-12 13:12:19 +0200

    using mapster for mapping adding carter in endpoint

--- src/Services/Catalog/Catalog.API/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
+++ src/Services/Catalog/Catalog.API/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
@@ -1,9 +1,15 @@
 ﻿namespace Catalog.API.Products.CreateProduct;
+using Carter;
+using Microsoft.AspNetCore.Routing;
 
 public record CreateProductRequest(string Name, List<string> Category, string Description, string ImageFile, decimal Price);
 
 public record CreateProductResponse(Guid Id);
 
-public class CreateProductEndpoint
+public class CreateProductEndpoint : ICarterModule
 {
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        throw new NotImplementedException();
+    }
 }
