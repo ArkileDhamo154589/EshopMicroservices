5d79cf5c4d5ae92ec9d81b1a3ee9ca5383f02b7a
dhamo <dhamo@MODULE_LAPTOP_1>
2024-11-12 12:58:44 +0200

    Refactor Product Handler using BuildingBlocks.CQRS change to ICommand , ICommandHanlder and in Handle we want the result, also commented steps in our task

--- src/Services/Catalog/Catalog.API/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,16 +1,22 @@
 ﻿using MediatR;
+using BuildingBlocks.CQRS;
+using System.Windows.Input;
 
 namespace Catalog.API.Products.CreateProduct;
 
 
 public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
-    : IRequest<CreateProductResult>;
+    : ICommand<CreateProductResult>;
 public record CreateProductResult(Guid Id);
 
-internal class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, CreateProductResult>
+internal class CreateProductCommandHandler
+    : ICommandHandler<CreateProductCommand, CreateProductResult>
 {
-    public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+    public Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
     {
+        //create product entity from command object
+        //save to database 
+        //return CreateProductResult result
         throw new NotImplementedException();
     }
 }
